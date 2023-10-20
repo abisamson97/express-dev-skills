69322759a6a9d9705be15a6800a6479a595c3ab3
Abi Samson <abi.samson97@yahoo.com>
2023-10-19 20:00:55 -0400

    Added Skills Function lab pt 2

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,17 +2,24 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkills
   };
+
+  function newSkills(req, res) {
+    res.render('skills/new', { title: 'New Skills' });
+  }
 
 function index(req, res) {
     res.render('skills/index', {
-      skills: Skill.getAll()
+      skills: Skill.getAll(),
+      title: 'All Skills'
     });
   };
 
   function show(req, res) {
     res.render('skills/show', {
       skill: Skill.getOne(req.params.id),
+      title: 'Skill Details'
     });
   }
--- routes/skills.js
+++ routes/skills.js
@@ -3,9 +3,12 @@ var router = express.Router();
 var skillsCtrl = require('../controllers/skills');
  // All actual paths start with "/skills"
 
+router.get('/new', skillsCtrl.new);
 // GET /skills
 router.get('/', skillsCtrl.index);
 // GET /skills/ :id
 router.get('/:id', skillsCtrl.show);
+// GET /todos/new
+
 
 module.exports = router;
